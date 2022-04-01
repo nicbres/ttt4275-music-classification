a9b76a59cd94951c815fd323674387fd8758d628
ivangushkov <69768720+ivangushkov@users.noreply.github.com>
2022-04-01 17:11:59 +0200

    The basic PCA implementation is done now (#5)
    * made a 3d scatter plotting function and a tester script
    
    * added PCA implementation

--- code/descriptive_statistics.py
+++ code/descriptive_statistics.py
@@ -1,10 +1,10 @@
 from data_handling import *
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
 
 X = read_genre_class_data(GENRE_CLASS_DATA_30S)
-
 cols = ["spectral_rolloff_mean", "mfcc_1_mean", "spectral_centroid_mean", "tempo"]
 
 
@@ -38,4 +38,38 @@ def correlations_plot(X, colset=None, colstart=None, colend=None):
     plt.title('Correlation Matrix', fontsize=16)
     plt.show()
 
-correlations_plot(X, colset=cols)
+def principal_components_reduction(X, genres, features=None, feature_start=None, feature_end=None):
+    
+    if features is not None:
+        X_PCA = X.loc[:,features]
+    elif feature_start is not None and feature_end is not None:
+        X_PCA = X.iloc[:,feature_start:feature_end]
+    else:
+        X_PCA = X.iloc[:,1:65]
+    
+    minmax_scaling = preprocessing.MinMaxScaler()
+    X_minmax = minmax_scaling.fit_transform(X_PCA)
+
+    #max_abs_scaler = preprocessing.MaxAbsScaler()
+    #X_maxabs = max_abs_scaler.fit_transform(X_PCA)
+
+    #X_std = (X_PCA - np.mean(X_PCA, axis=0))/np.std(X_PCA, axis=0)
+    U, S, VT = np.linalg.svd(X_minmax, full_matrices=False)    
+
+    Sigma = np.diag(S)
+    T = U @ Sigma
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+
+    ax.set_xlabel("Component 1")
+    ax.set_ylabel("Component 2")
+
+    ax.grid()
+
+    for genre in genres:
+        genre_inds = np.where(X['Genre'] == genre)
+        ax.scatter(T[genre_inds, 0],T[genre_inds, 1],label=f"{genre}")
+    
+    ax.legend()
+    plt.show()
--- code/tester.py
+++ code/tester.py
@@ -1,9 +1,13 @@
 from plotting import *
 from data_handling import *
 
+from descriptive_statistics import *
+
 X = read_genre_class_data(GENRE_CLASS_DATA_30S)
 
-genres = ["blues", "metal", "jazz", "classical", "rock"]
-features = ["spectral_rolloff_mean", "mfcc_1_mean", "spectral_centroid_mean"]
+genres = ["blues", "metal", "jazz", "classical", "rock", "pop", "country", "hiphop", "reggae", "disco"]
+#features = ["spectral_rolloff_mean", "tempo", "spectral_centroid_mean"]
 
-threed_scatter(X, features=features, genres=genres)
+
+principal_components_reduction(X, genres)
+#threed_scatter(X, features=features, genres=genres)
