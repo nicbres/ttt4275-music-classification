f02310e9ab827b4dfc0620f5cd70d319b97801bc
ivangushkov <ivanig@stud.ntnu.no>
2022-04-02 12:28:24 +0200

    Initialize the diy branch

--- code/tester.py
+++ code/tester.py
@@ -2,13 +2,12 @@ from plotting import *
 from data_handling import *
 from descriptive_statistics import *
 
-from descriptive_statistics import *
+X = read_genre_class_data(GENRE_CLASS_DATA_5S)
 
-X = read_genre_class_data(GENRE_CLASS_DATA_30S)
-
-genres = ["blues", "metal", "jazz", "classical", "rock", "pop", "country", "hiphop", "reggae", "disco"]
-#features = ["spectral_rolloff_mean", "tempo", "spectral_centroid_mean"]
+genres = ["metal", "jazz", "classical", "pop"]
+features = ["spectral_rolloff_mean", "tempo", "spectral_centroid_mean"]
 
 
 principal_components_reduction(X, genres)
-#threed_scatter(X, features=features, genres=genres)
+threed_scatter(X, features=features, genres=genres)
+
