559ffb90f34ef2a6233ddbe41db5199127b66150
ivangushkov <69768720+ivangushkov@users.noreply.github.com>
2022-04-05 11:35:31 +0200

    Add the finished (slow) kNN implementation (#8)
    * Add PCA implementation
    
    * Initialize the diy branch
    
    * Start the diy kNN classifier
    
    * Finish kNN classifier with generic p_norm

---
+++ code/diy_classifiers.py
@@ -0,0 +1,64 @@
+from pyexpat import features
+import numpy as np
+import heapq
+from statistics import mode
+
+
+def p_norm(p, vec):
+    vec_powered = [vec[i]**p for i in range(len(vec))]
+    return (sum(vec_powered)) ** 1/p
+
+def diy_kNN(k,
+            data_frame, 
+            test_frame,
+            feature_set,
+            p=None,
+            distance_metric=None
+            ):
+    """
+    Inputs:     k - number of Nearest Neighbours to be considered
+                data_frame - pandas dataframe of the training set
+                test_frame - pandas dataframe of the test set to be classified
+                feature_set - a list of the features to be used in the classifier
+                p - optional arg for order of distance norm
+                distance_metric - optional arg for playing with different disdence matrics
+    """
+
+    if p is None:
+        p = 2
+    
+    # TODO: play around with other metrics
+
+    N_train = np.shape(data_frame)[0]
+    N_test = np.shape(test_frame)[0]
+
+    training_table = data_frame[feature_set]
+    training_genres = data_frame["Genre"]
+
+    test_table = test_frame[feature_set]
+    predicted_genres = np.empty((N_test, ), dtype=object)
+
+    for i in range(N_test):
+        
+        # Compute the euclidean distances to all training points
+        distances = [p_norm(p,test_table.iloc[i]-training_table.iloc[j]) for j in range(N_train)]
+
+        # Find the k smallest distances
+        k_smallest = heapq.nsmallest(int(k), distances)
+
+        # Extract their indices
+        kNN_inds = [distances.index(k_smallest[j]) for j in range(len(k_smallest))]
+
+        # Extract the Genres of the distance_inds from the training data_frame
+        genres_of_kNN = training_genres[kNN_inds]
+
+        # Pick the most frequent genre
+        most_frequent_neighbour = mode(genres_of_kNN)
+
+        # Assign that genre to the track
+        predicted_genres[i] = most_frequent_neighbour
+
+    return predicted_genres
+
+
+    
--- code/tester.py
+++ code/tester.py
@@ -1,13 +1,22 @@
 from plotting import *
 from data_handling import *
-
 from descriptive_statistics import *
+from diy_classifiers import *
 
 X = read_genre_class_data(GENRE_CLASS_DATA_30S)
 
-genres = ["blues", "metal", "jazz", "classical", "rock", "pop", "country", "hiphop", "reggae", "disco"]
-#features = ["spectral_rolloff_mean", "tempo", "spectral_centroid_mean"]
+vec1 = np.array([8056.984614, 112.347147, 3889.624969])
+vec2 = np.array([7633.162177, 89.102909, 3727.466037])
+print(np.linalg.norm(vec1 - vec2))
 
+train_frame = X[X["Type"] == "Train"]
+test_frame = X[X["Type"] == "Test"]
 
-principal_components_reduction(X, genres)
+k = 8
+genres = ["metal", "jazz", "classical", "pop"]
+features = ["spectral_rolloff_mean", "tempo", "spectral_centroid_mean", "mfcc_1_mean"]
+
+diy_kNN(k, train_frame, test_frame, features, p=4)
+#principal_components_reduction(X, genres)
 #threed_scatter(X, features=features, genres=genres)
+
