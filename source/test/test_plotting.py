5baff82e1faf42e89591662a6c79f64aff1094e6
Nicholas Bresina <nicholdb@stud.ntnu.no>
2022-04-08 12:48:04 +0200

    Add example sklearn pipeline

--- source/test/test_plotting.py
+++ source/test/test_plotting.py
@@ -57,6 +57,29 @@ def test_confusion_matrix_plot_sklearn_svm_reference(features):
     )
 
 
+@pytest.mark.parametrize("features",
+    [
+        source.mappings.MUSIC_FEATURES_ALL,
+        source.mappings.get_features_from_indices([11, 42, 7, 41]),
+    ],
+)
+def test_confusion_matrix_plot_sklearn_pipeline_reference(features):
+    data = source.data_handling.read_genre_class_data(
+        file_path=source.data_handling.GENRE_CLASS_DATA_30S,
+    )
+
+    training_data, test_data = source.data_handling.prepare_data(
+        data_frame=data,
+        features=features,
+    )
+
+    predicted_genres = source.sklearn_reference.Pipeline.predict(training_data, test_data)
+
+    source.plotting.confusion_matrix(
+        actual_genres=test_data.y,
+        predicted_genres=predicted_genres,
+    )
+
 
 @pytest.mark.parametrize("features",
     [
